3c78a143d6f2a7f3711d2a588070600113f4c645
Piyush Srivastava <piyush.0101@gmail.com>
2013-09-20 23:42:49 -0500

    adding a jersey-bundle dependency

--- src/main/java/com/thoughtworks/sensors/client/SensorWebServiceClient.java
+++ src/main/java/com/thoughtworks/sensors/client/SensorWebServiceClient.java
@@ -9,7 +9,7 @@ public class SensorWebServiceClient implements SensorClient {
     @Override
     public Double getTemperature() {
         Client client = Client.create();
-        WebResource resource = client.resource("http://192.168.0.14:8080/temperature");
+        WebResource resource = client.resource("http://192.168.0.8:8080/temperature");
         ClientResponse response = resource.accept("application/txt").get(ClientResponse.class);
 
         String output = response.getEntity(String.class);
@@ -20,7 +20,7 @@ public class SensorWebServiceClient implements SensorClient {
     @Override
     public Double getLight() {
         Client client = Client.create();
-        WebResource resource = client.resource("http://192.168.0.14:8080/light");
+        WebResource resource = client.resource("http://192.168.0.8:8080/light");
         ClientResponse response = resource.accept("application/txt").get(ClientResponse.class);
 
         String output = response.getEntity(String.class);
